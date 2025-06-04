21167de170b5f5358471e1dea8b3797cd52b08ee
nuhajoy <nuhaminemnebered@gmail.com>
2025-06-04 06:57:09 +0300

    final commit

--- Amazon-Clone/amazon-clone/src/Api/axios.js
+++ Amazon-Clone/amazon-clone/src/Api/axios.js
@@ -1,7 +1,13 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:5001/clone-8c1c7/us-central1/api", // ✅ Correct format
+
+  // local instance for firebase
+  // baseURL: "http://127.0.0.1:5001/clone-8c1c7/us-central1/api",
+
+  // deployed version of amazon server on render 
+
+  baseURL: "https://amazon-clone-2025-new.onrender.com",
 });
 
 export { axiosInstance };
