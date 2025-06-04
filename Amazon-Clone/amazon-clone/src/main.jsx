b0c7cfba2d5ce10fac78b8131ba4dfb336af45a6
nuhajoy <nuhaminemnebered@gmail.com>
2025-06-04 07:24:30 +0300

    final commit

--- Amazon-Clone/amazon-clone/src/main.jsx
+++ Amazon-Clone/amazon-clone/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx';
-import { DataProvider } from './components/DataProvider/DataProvider.jsx';
+import { DataProvider } from './components/DataProvider/DataProvider';
 import {reducer,initialState} from './utiltiy/Reducer.jsx'
 
 createRoot(document.getElementById('root')).render(
